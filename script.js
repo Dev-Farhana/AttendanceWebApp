3a4cd2e1cb02403ed5cacdb49da12d76e0d5a3d3
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-10-07 12:36:44 +0500

    incomplete.

---
+++ script.js
@@ -0,0 +1,9 @@
+function addClass() {
+    // Add class details logic here
+    alert('Class added successfully');
+}
+
+function markAttendance() {
+    // Mark attendance logic here
+    alert('Attendance marked successfully');
+}
